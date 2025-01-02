dac4e8d4c544ac2bece750b80e35408e1d8a8c12
aravind <aravind284479@gmail.com>
2025-01-02 21:23:13 +0530

    Add first workflow

--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -4,7 +4,12 @@ import { StatusBar } from "expo-status-bar";
 import { COLORS } from "../global/constants";
 
 const Wrapper = ({ children }) => {
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      {children}
+    </SafeAreaView>
+  );
 };
 
 export default Wrapper;
